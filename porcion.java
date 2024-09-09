204f1fc54e0f64181e984de248b8b7f2bd34eab3
Israel <olveraisrael942@gmail.com>
2024-09-08 20:39:00 -0600

    agregue tres lineas de codigpo

--- porcion.java
+++ porcion.java
@@ -1,5 +1,10 @@
 public class {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
+        int a = 5;
+        int b = 10;
+        int c = a + b;
+        
     }
+
 }
